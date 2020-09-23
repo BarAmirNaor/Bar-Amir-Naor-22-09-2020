084c55fddcaa2161974c2499e1f57276e1124d80
Bar Amir <bar@shookit.online>
2020-09-23 12:15:42 +0300

    Updates

---
+++ precache-manifest.5723ee27e4cb5da71cecc19810229b67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff73a1e9a8a0f7fc0c5a545483fe0d60",
+    "url": "/Bar-Amir-Naor-22-09-2020/index.html"
+  },
+  {
+    "revision": "b85d26c071baebb341e9",
+    "url": "/Bar-Amir-Naor-22-09-2020/static/js/2.97063bf6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Bar-Amir-Naor-22-09-2020/static/js/2.97063bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cfcfde0ebad8b743568",
+    "url": "/Bar-Amir-Naor-22-09-2020/static/js/main.0ecdeeb2.chunk.js"
+  },
+  {
+    "revision": "e82bdb6fff8d06ddf82f",
+    "url": "/Bar-Amir-Naor-22-09-2020/static/js/runtime-main.654551ca.js"
+  }
+]);
